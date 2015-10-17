e46b923078ad3cda57b8f87a987a31c77665ddbc
Japheth Balane <japheth162@gmail.com>
2015-10-18 00:36:13 +0800

    After image added

--- script.js
+++ script.js
@@ -5,10 +5,11 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 var points1 = [];
-// var points2 = [];
+var points2 = [];
+var points3 = [];
 
 setInterval(world, 30);
-generatePoints(4);
+generatePoints(20);
 
 function randomBetween(min, max) {
 	return Math.floor(Math.random() * (max - min)) + min;
@@ -17,8 +18,9 @@ function randomBetween(min, max) {
 function generatePoints(count) {
 	if (count > 50) {count = 50};
 	for (var i = 0; i < count; i++) {
-		points1.push(new Point());
-		// points2.push(new Point());
+		points1.push(new Point(0,canvas.width/3));
+		points2.push(new Point(canvas.width/3,canvas.width/3*2));
+		points3.push(new Point(canvas.width/3*2,canvas.width));
 	};
 }
 
@@ -26,13 +28,15 @@ function world() {
 	clearCanvas();
 	drawLines();
 	for (var i = 0; i < points1.length; i++) {
-		points1[i].update(0,canvas.width/2).draw();
-		// points2[i].update(canvas.width/2,canvas.width).draw();
+		points1[i].update().draw();
+		points2[i].update().draw();
+		points3[i].update().draw();
 	}
 }
 
 function clearCanvas() {
-	context.fillStyle = "rgba(155,155,155,0.5)";
+	context.fillStyle = "rgba(0,0,0,0.1)";
+	// context.fillStyle = "rgba(255,255,255,0.5)";
 	context.fillRect(0,0,canvas.width,canvas.height);
 }
 
@@ -40,22 +44,29 @@ function drawLines() {
 	for (var i = 0; i < points1.length; i++) {
 		for (var j = i+1; j < points1.length; j++) {
 			context.beginPath();
+			context.strokeStyle = "#ff0000";
 			context.moveTo(points1[i].x, points1[i].y);
 			context.lineTo(points1[j].x, points1[j].y);
 			context.stroke();
+			context.beginPath();
+			context.strokeStyle = "#00ff00";
+			context.moveTo(points2[i].x, points2[i].y);
+			context.lineTo(points2[j].x, points2[j].y);
+			context.stroke();
+			context.beginPath();
+			context.strokeStyle = "#0000ff";
+			context.moveTo(points3[i].x, points3[i].y);
+			context.lineTo(points3[j].x, points3[j].y);
+			context.stroke();
 		}
-		// for (var j = i+1; j < points1.length; j++) {
-		// 	context.beginPath();
-		// 	context.moveTo(points2[i].x, points2[i].y);
-		// 	context.lineTo(points2[j].x, points2[j].y);
-		// 	context.stroke();
-		// }
 	}
 }
 
-function Point() {
-	this.x = randomBetween(0, canvas.width);
+function Point(p1,p2) {
+	this.x = randomBetween(p1, p2);
 	this.y = randomBetween(0, canvas.height);
+	// this.x = randomBetween(0, canvas.width);
+	// this.y = randomBetween(0, canvas.height);
 	this.radius = 3;
 	this.speedx = randomBetween(2, 5);
 	this.speedy = randomBetween(1, 5);
@@ -71,12 +82,19 @@ function Point() {
 		this.x+=this.speedx;
 		this.y+=this.speedy;
 
-		if (this.x < 0 || this.x > canvas.width) {
+		if (this.x < p1 || this.x > p2) {
 			this.speedx*=-1;
 		};
 		if (this.y < 0 || this.y > canvas.height) {
 			this.speedy*=-1;
 		};
+
+		// if (this.x < 0 || this.x > canvas.width) {
+		// 	this.speedx*=-1;
+		// };
+		// if (this.y < 0 || this.y > canvas.height) {
+		// 	this.speedy*=-1;
+		// };
 
 		return this;
 	}
@@ -86,8 +104,8 @@ function Point() {
 		context.fillStyle = "#000000";
 		context.strokeStyle = "rgba(0,0,0,0.5)";
 		context.arc(this.x, this.y, this.radius, Math.PI * 2, false);
-		context.stroke();
-		context.fill();
+		// context.stroke();
+		// context.fill();
 
 		return this;	
 	}
